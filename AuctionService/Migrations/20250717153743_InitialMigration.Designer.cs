cfac556d1ef90f627e3d7bd68d7bab09501b650c
Dimitrije <dimitrije219@gmail.com>
2025-07-18 01:00:21 +0200

    Code review and created a new migration

--- AuctionService/Migrations/20250717153743_InitialMigration.Designer.cs
+++ AuctionService/Migrations/20250717153743_InitialMigration.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AuctionService.Migrations
 {
     [DbContext(typeof(AuctionDbContext))]
-    [Migration("20250606125140_InitialMigration")]
+    [Migration("20250717153743_InitialMigration")]
     partial class InitialMigration
     {
         /// <inheritdoc />
@@ -82,7 +82,7 @@ namespace AuctionService.Migrations
                             MyProperty = 0,
                             ReservePrice = 20000,
                             Seller = "bob",
-                            Status = 0,
+                            Status = 1,
                             UpdatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
@@ -94,7 +94,7 @@ namespace AuctionService.Migrations
                             MyProperty = 0,
                             ReservePrice = 90000,
                             Seller = "alice",
-                            Status = 0,
+                            Status = 1,
                             UpdatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
@@ -106,7 +106,7 @@ namespace AuctionService.Migrations
                             MyProperty = 0,
                             ReservePrice = 0,
                             Seller = "bob",
-                            Status = 0,
+                            Status = 1,
                             UpdatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
@@ -130,13 +130,13 @@ namespace AuctionService.Migrations
                             MyProperty = 0,
                             ReservePrice = 20000,
                             Seller = "alice",
-                            Status = 0,
+                            Status = 1,
                             UpdatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
                             Id = new Guid("dc1e4071-d19d-459b-b848-b5c3cd3d151f"),
-                            AuctionEnd = new DateTime(2025, 6, 15, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            AuctionEnd = new DateTime(2025, 8, 15, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             CreatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             ItemId = new Guid("a2510c14-f119-4717-b4d5-c3eefe4b5782"),
                             MyProperty = 0,
@@ -148,7 +148,7 @@ namespace AuctionService.Migrations
                         new
                         {
                             Id = new Guid("47111973-d176-4feb-848d-0ea22641c31a"),
-                            AuctionEnd = new DateTime(2025, 5, 13, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            AuctionEnd = new DateTime(2025, 9, 13, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             CreatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             ItemId = new Guid("d3f9cf03-8a07-493d-9945-2cba4e3e1360"),
                             MyProperty = 0,
@@ -172,7 +172,7 @@ namespace AuctionService.Migrations
                         new
                         {
                             Id = new Guid("40490065-dac7-46b6-acc4-df507e0d6570"),
-                            AuctionEnd = new DateTime(2025, 5, 20, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            AuctionEnd = new DateTime(2025, 11, 20, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             CreatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             ItemId = new Guid("d90c328e-a26c-492d-b7c5-3ec037d1919d"),
                             MyProperty = 0,
@@ -184,7 +184,7 @@ namespace AuctionService.Migrations
                         new
                         {
                             Id = new Guid("3659ac24-29dd-407a-81f5-ecfe6f924b9b"),
-                            AuctionEnd = new DateTime(2025, 6, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                            AuctionEnd = new DateTime(2025, 10, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             CreatedAt = new DateTime(2025, 4, 29, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             ItemId = new Guid("9be53e24-81e5-4e38-ab15-c14b2920c8ea"),
                             MyProperty = 0,
--- SearchService/Services/Implementation/SearchItems.cs
+++ SearchService/Services/Implementation/SearchItems.cs
@@ -54,7 +54,7 @@ namespace SearchService.Services.Implementation
             // Ako je već SortByTextScore pozvan ranije, dodatni sort neće imati efekta osim ako nije poništen
             return orderBy switch
             {
-                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "make" => query.Sort(x => x.Ascending(a => a.Make)).Sort(x=>x.Ascending(a=>a.Model)),
                 "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
                 _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
             };
